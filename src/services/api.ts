45088c344be8205dde39b3bf6df7f88f28ede01d
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-10-20 17:28:04 -0300

    feat: adding axios and create api for pokeapi and creating standart request

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "https://pokeapi.co/api/v2/",
+    timeout: 6000,
+})
+
+interface iRequest<Data>{
+    method: "post" | "get" | "put" | "patch" | "delete"
+    url: string;
+    data?: Data;
+    headers?: any;
+}
+
+export const request = async (req: iRequest<any>) => {
+    const response = await axios.request(req)
+    return response;
+}
