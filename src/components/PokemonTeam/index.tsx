e68a963dbcac2b35d861e0af197fa1423fb01a28
Alex Conder <alex.conder@nerdweb.com.br>
2022-12-05 10:30:55 -0300

    feat: outclick in favorite list

--- src/components/PokemonTeam/index.tsx
+++ src/components/PokemonTeam/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "use-context-selector";
 import { PokeTeamContext } from "../../contexts/PokeTeamContext";
 import { useDropZone } from "../../hooks/useDropZone";
