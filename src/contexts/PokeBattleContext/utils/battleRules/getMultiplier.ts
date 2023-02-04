7fdbcbb5027c3d6f8ded868dd0e8a4f9ae7c8fd6
Alex Conder <alex.v.conder@gmail.com>
2023-02-04 08:47:21 -0300

    refactor: is superEffective in getMultiplier

--- src/contexts/PokeBattleContext/utils/battleRules/getMultiplier.ts
+++ src/contexts/PokeBattleContext/utils/battleRules/getMultiplier.ts
@@ -1,26 +1,18 @@
 import { types } from "../../../../data/pokemonTypes";
 import { iType } from "../../../types";
 
-export function isSuperEffective(moveType: string, pokemonTypes: iType[]) {
-  let modifier = 0;
-
+export function getMultiplier(moveType: string, pokemonTypes: iType[]) {
   pokemonTypes.forEach((type) => {
     const currentMoveType = types.find((t) => {
       return t.type === type.type.name;
     });
 
     if (currentMoveType?.halfDamage.includes(moveType)) {
-      modifier = modifier - 1;
+      return .5;
     } else if (currentMoveType?.doubleDamage.includes(moveType)) {
-      modifier = modifier + 1;
+      return 2;
+    } else {
+      return 1;
     }
   });
-
-  if (modifier > 0) {
-    return "super-effective";
-  } else if (modifier < 0) {
-    return "not-very-effective";
-  } else {
-    return false;
-  }
 }
