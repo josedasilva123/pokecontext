feb381101959c55949651ad5a9fb07e55dca8dd6
Alex Conder <alex.v.conder@gmail.com>
2023-02-04 11:34:31 -0300

    identation

--- src/contexts/PokeBattleContext/utils/battleRules/getMultiplier.ts
+++ src/contexts/PokeBattleContext/utils/battleRules/getMultiplier.ts
@@ -10,16 +10,16 @@ export function getMultiplier(moveType: string, pokemonTypes: iType[]) {
       });
 
       if (currentMoveType?.halfDamage.includes(moveType)) {
-         modifier = modifier + 1;
-      } else if (currentMoveType?.doubleDamage.includes(moveType)) {
          modifier = modifier - 1;
+      } else if (currentMoveType?.doubleDamage.includes(moveType)) {
+         modifier = modifier + 1;
       }
    });
 
    if (modifier > 0) {
       return { modifier: 2, message: "Foi super efetivo!" };
    } else if (modifier < 0) {
-      return { modifier: .5, message: "Não é muito efeitivo!" };
+      return { modifier: 0.5, message: "Não é muito efeitivo!" };
    } else {
       return { modifier: 1 };
    }
--- src/contexts/PokeBattleContext/utils/battleRules/isPhysicalOrSpecial.ts
+++ src/contexts/PokeBattleContext/utils/battleRules/isPhysicalOrSpecial.ts
@@ -1,16 +1,16 @@
-export function isPhysicalOrSpecial(moveCategory: string){
-    let attack: number;
-    let defense: number;
-    if (moveCategory === "physical") {
+export function isPhysicalOrSpecial(moveCategory: string) {
+   let attack: number;
+   let defense: number;
+   if (moveCategory === "physical") {
       attack = 1;
       defense = 2;
-    } else {
+   } else {
       attack = 3;
       defense = 4;
-    }
+   }
 
-    return {
-        attack,
-        defense,
-    }
+   return {
+      attack,
+      defense,
+   };
 }
