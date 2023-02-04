35b89d4bfdeb5a535dcb5f17a7b2b5b533f1f60f
Alex Conder <alex.v.conder@gmail.com>
2023-02-04 11:34:59 -0300

    feat: get stats with multiplier buff or debuff

---
+++ src/contexts/PokeBattleContext/utils/battleRules/getStatsMultiplier.ts
@@ -0,0 +1,33 @@
+import { iPokemonBattleStat } from "../../../types";
+import { iStatsMultiplier } from "../../reducers/types";
+
+export function getStatsMultiplier(statsIndex: number, statsMultiplier: iStatsMultiplier) {
+    switch (statsIndex) {
+       case 1:
+          return statsMultiplier.attack;
+       case 2:
+          return statsMultiplier.defense;
+       case 3:
+          return statsMultiplier.specialAttack;
+       case 4:
+          return statsMultiplier.specialDefense;
+       case 5:
+          return statsMultiplier.speed;
+    }
+ }
+
+ export function getStatsWithMultiplier(stats: iPokemonBattleStat, statsIndex: number, statsMultiplier: iStatsMultiplier){
+    const multiplier = getStatsMultiplier(statsIndex, statsMultiplier) as number;
+
+    if(multiplier > 0){
+        const statusBuff = stats.value * .5 * multiplier;
+        return stats.value + statusBuff;
+    } else if (multiplier < 0){
+        const statsDebuff = 2 / ( multiplier + 2 )
+        return stats.value * statsDebuff;
+    } else {
+        console.log(stats.value);
+        return stats.value;
+    }
+    
+ }
