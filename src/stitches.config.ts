b6ed0aa33670469e1e1ddd24bc15fb598cb6b6f1
Alex Conder <alex.v.conder@gmail.com>
2023-02-05 12:21:14 -0300

    fix: moving bp config to the right place

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -1,25 +1,25 @@
 import { createStitches } from "@stitches/react";
 
 export const { styled, keyframes, globalCss } = createStitches({
-    theme: {
-        colors: {
-            primary: '#ee6b2f',
-            gray2: '#DEDEDE',
-            gray1: '#FAFAFA',
-            white: '#ffffff',
-            black: '#000000',
-        },
-        fonts: {
-            primary: 'Roboto',
-            secondary: 'Poppins'
-        },
-        media: {
-            bp1: '(max-width: 1368px)',
-            bp2: '(max-width: 1024px)',
-            bp3: '(max-width: 900px)',
-            bp4: '(max-width: 768px)',
-            bp5: '(max-width: 450px)',
-            bp6: '(max-width: 375px)',
-        }        
-    }
-})
+   theme: {
+      colors: {
+         primary: "#ee6b2f",
+         gray2: "#DEDEDE",
+         gray1: "#FAFAFA",
+         white: "#ffffff",
+         black: "#000000",
+      },
+      fonts: {
+         primary: "Roboto",
+         secondary: "Poppins",
+      },
+   },
+   media: {
+      bp1: "(max-width: 1368px)",
+      bp2: "(max-width: 1024px)",
+      bp3: "(max-width: 900px)",
+      bp4: "(max-width: 768px)",
+      bp5: "(max-width: 450px)",
+      bp6: "(max-width: 375px)",
+   },
+});
