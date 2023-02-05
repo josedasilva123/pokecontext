41fa47cce29672712be9c7f7923e7b2f927564ee
Alex Conder <alex.v.conder@gmail.com>
2023-02-05 12:11:34 -0300

    feat: configuring default breakpoints in stitches theme

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -12,6 +12,14 @@ export const { styled, keyframes, globalCss } = createStitches({
         fonts: {
             primary: 'Roboto',
             secondary: 'Poppins'
+        },
+        media: {
+            bp1: '(max-width: 1368px)',
+            bp2: '(max-width: 1024px)',
+            bp3: '(max-width: 900px)',
+            bp4: '(max-width: 768px)',
+            bp5: '(max-width: 450px)',
+            bp6: '(max-width: 375px)',
         }        
     }
 })
