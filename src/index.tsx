35d2ca374c09d9ae83488428b7952ebb492e7e00
Alex Conder <alex.v.conder@gmail.com>
2023-02-05 11:32:50 -0300

    removing unsed import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import Providers from "./contexts/Providers";
 import { BrowserRouter } from "react-router-dom";
 
